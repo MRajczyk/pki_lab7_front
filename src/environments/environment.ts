6e225662ae3376d4cabe47d4b18a94ad2fa3b3ad
MRajczyk <mikolajrajczyk01@gmail.com>
2023-04-29 12:47:30 +0200

    added handling backend hosted in cloud

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  url: 'http://localhost:8080',
+  url: 'https://pki-lab7-back.onrender.com',
   production: false
 };
 
